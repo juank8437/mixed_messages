b5aee6d0898c08ff6711d7cf36a7c3b57b56c1e6
Juan Carlos Rodriguez <juank8437@hotmail.com>
2022-05-24 15:36:34 -0400

    Store messsage, objet message creation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('script.js file');
+// Mixed Messages Creator
+const message = {
+    intro: ["Hola", "Buenos dias", "Buenas tardes", "Buenas noches"],
+    middle: ["Como estas?", "Como le va?", "Como le ha ido?", "Como ha estado?"],
+    end: ["Que gusto verlo", "Un placer encontralo neuvamente", "Feliz de verlo", "Contento de verlo"]
+}
