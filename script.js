1de1dd502ad3f7025162b7307f4daf17c69122f3
Juan Carlos Rodriguez <juank8437@hotmail.com>
2022-05-24 16:39:36 -0400

    First functional release

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Mixed Messages Creator
 const message = {
     intro: ["Hola", "Buenos dias", "Buenas tardes", "Buenas noches"],
-    middle: ["Como estas?", "Como le va?", "Como le ha ido?", "Como ha estado?"],
-    end: ["Que gusto verlo", "Un placer encontralo neuvamente", "Feliz de verlo", "Contento de verlo"]
+    middle: ["como estas?", "como le va?", "como le ha ido?", "como ha estado?"],
+    end: ["que gusto verlo", "un placer encontralo nuevamente", "feliz de verlo", "contento de verlo"]
 }
+let generateMessage = (intro,middle,end) => console.log(intro+", "+middle+", "+end)
+
+let intro = message.intro[Math.floor(Math.random()*message.intro.length)]
+let middle = message.middle[Math.floor(Math.random()*message.middle.length)]
+let end = message.end[Math.floor(Math.random()*message.end.length)]
+
+generateMessage(intro,middle,end)
